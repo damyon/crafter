3db59315e123575fde83b56fbbae70492137cdd4
Damyon Wiese <damyon.wiese@gmail.com>
2025-10-07 08:30:14 +0800

    Speed

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use glium::Surface;
 use glium::winit::event::ElementState;
 use glium::winit::event::MouseButton;
 use glium::winit::event_loop::ControlFlow;
+use std::time::Instant;
 mod graphics;
 
 mod camera;
@@ -25,6 +26,7 @@ mod vertex;
 
 fn main() {
     let mut scene = Scene::new();
+    const TARGET_FPS: u64 = 60;
 
     scene.init();
     // We start by creating the EventLoop, this can only be done once per process.
@@ -32,24 +34,16 @@ fn main() {
     let event_loop = glium::winit::event_loop::EventLoop::builder()
         .build()
         .expect("event loop building");
+
     let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
         .with_title("Crafter")
         .with_inner_size(800, 600)
         .build(&event_loop);
 
-    // Start rendering by creating a new frame
-    let mut frame = display.draw();
-    // Which we fill with an opaque blue color
-    frame.clear_color(0.3, 0.0, 1.0, 1.0);
-    // By finishing the frame swap buffers and thereby make it visible on the window
-    frame.finish().unwrap();
-
-    // Now we wait until the program is closed
     #[allow(deprecated)]
     event_loop
         .run(move |event, window_target| {
-            window_target.set_control_flow(ControlFlow::Poll);
-
+            let start_time = Instant::now();
             match event {
                 glium::winit::event::Event::WindowEvent { event, .. } => match event {
                     // This event is sent by the OS when you close the Window, or request the program to quit via the taskbar.
@@ -57,7 +51,9 @@ fn main() {
                     glium::winit::event::WindowEvent::Resized(window_size) => {
                         display.resize(window_size.into());
                     }
+
                     glium::winit::event::WindowEvent::RedrawRequested => {
+                        let start = Instant::now();
                         let mut frame = display.draw();
                         let (width, height) = display.get_framebuffer_dimensions();
                         let mut graphics: Graphics =
@@ -66,8 +62,8 @@ fn main() {
                         // By finishing the frame swap buffers and thereby make it visible on the window
                         scene.draw(&mut graphics);
                         frame.finish().unwrap();
-
-                        window.request_redraw();
+                        let end = Instant::now();
+                        println!("Frame time: {:?}", end - start);
                     }
                     glium::winit::event::WindowEvent::MouseInput {
                         device_id,
@@ -85,7 +81,7 @@ fn main() {
                                         data2: 1,
                                     };
                                     scene.queue_command(mouse_down);
-                                    window.request_redraw();
+                                    scene.process_commands();
                                 }
                                 _ => {}
                             },
@@ -97,7 +93,7 @@ fn main() {
                                         data2: 1,
                                     };
                                     scene.queue_command(mouse_up);
-                                    window.request_redraw();
+                                    scene.process_commands();
                                 }
                                 _ => {}
                             },
@@ -115,12 +111,11 @@ fn main() {
                             data2: position.y as u32,
                         };
                         scene.queue_command(mouse_moved);
-                        window.request_redraw();
+                        scene.process_commands();
                     }
                     _ => (),
                 },
                 glium::winit::event::Event::AboutToWait => {
-                    scene.process_commands();
                     window.request_redraw();
                 }
                 _ => (),
